bcd841114b4f335b4f22fdd75fb024609b0c13de
Shawn Throop <shawnthroop@gmail.com>
2018-05-21 16:55:22 +0200

    Bumped copyright year, fixed header comments.

--- Sources/Peanut.h
+++ Sources/Peanut.h
@@ -1,18 +1,17 @@
 //
-//  Peanut_iOS.h
-//  Peanut iOS
+//  Peanut.h
+//  Peanut
 //
 //  Created by Shawn Throop on 21.05.18.
 //
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for Peanut_iOS.
+//! Project version number for Peanut.
 FOUNDATION_EXPORT double PeanutVersionNumber;
 
-//! Project version string for Peanut_iOS.
+//! Project version string for Peanut.
 FOUNDATION_EXPORT const unsigned char PeanutVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <Peanut_iOS/PublicHeader.h>
-
+// In this header, you should import all the public headers of your framework using statements like #import <Peanut/PublicHeader.h>
 
